95dbc166b64699c3e3abcd36a094b99207ffcc8b
WangQiang <2358164733@qq.com>
2022-04-10 20:20:15 +0800

    啥

---
+++ lalal.vue
@@ -0,0 +1,14 @@
+
+
+new vue(
+    {data:{
+        name: "王强"，
+        methods:{
+            show:function fn(){
+                 console.log("show method");
+            }
+        }
+
+    }
+    }
+)
