23000898fb2982a3a606d4c7deadd60e2dd9d630
wangqiang <2358164733@qq.com>
2022-07-16 22:11:37 +0800

    修改了vue文件

---
+++ 537.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: '537',
+
+  data () {
+    return {
+         name:"wangqiang",
+         age:23
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
